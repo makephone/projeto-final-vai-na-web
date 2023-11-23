03ad35676ccaebd8d334040f01bf986ed9b3b3ee
makephone <t.decarvalhosilva@gmail.com>
2023-11-23 08:00:01 -0300

    pequenas imagens

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import  React from 'react'
+import foto from "./avatar.png"
 import './App.css'
-
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
+     <header>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <figure>
+        <img src={foto} alt='foto de perfil' title='Tiago De Carvalho'/>
+        <figcaption>Tiago Silva</figcaption>
+        </figure>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Desenvolvedor Frontend</h1>
+     </header>
+     <main>
+      <section>
+      <div className='slide'>
+      <button>/n</button>
+
+      <button>/p</button>
+        
+      </div> 
+        
+        
+
+
+      </section>
+     </main>
+     <footer>
+      <p>rodape</p>
+     </footer>
     </>
   )
 }
